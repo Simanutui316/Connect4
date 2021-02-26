e486c899da4d0d4269b368d8c24ed4f8f10f21bf
Justin TuiSamoa <justintuisamoa@Justins-MacBook-Pro.local>
2021-02-26 08:23:12 -0800

    Testing README

--- main.js
+++ main.js
@@ -53,13 +53,13 @@ boardEl.addEventListener('click', function (e) {
     if (e.target.className !== 'pos') {
         return
     }
-    // console.log(e.target.id)
-    let row
-    // console.log(row);
-    let column = parseInt(e.target.id) % 7;
-    // console.log(column);
 
-    // row that is the most bottom that is zero
+    let row
+
+    let column = parseInt(e.target.id) % 7;
+
+
+
     for (let i = board.length - 1; i >= 0; i--) {
         if (board[i][column] === 0) {
             row = i
@@ -160,30 +160,28 @@ boardEl.addEventListener('click', function init() {
 
 
 function chkLine(a, b, c, d) {
-    // Check first cell non-zero and all cells match
+
     return ((a != 0) && (a == b) && (a == c) && (a == d));
 }
 
 function chkWinner(bd) {
-    // Check down
+
     for (r = 0; r < 3; r++)
         for (c = 0; c < 7; c++)
             if (chkLine(bd[r][c], bd[r + 1][c], bd[r + 2][c], bd[r + 3][c]))
                 return bd[r][c];
 
-    // Check right
+
     for (r = 0; r < 6; r++)
         for (c = 0; c < 4; c++)
             if (chkLine(bd[r][c], bd[r][c + 1], bd[r][c + 2], bd[r][c + 3]))
                 return bd[r][c];
 
-    // Check down-right
     for (r = 0; r < 3; r++)
         for (c = 0; c < 4; c++)
             if (chkLine(bd[r][c], bd[r + 1][c + 1], bd[r + 2][c + 2], bd[r + 3][c + 3]))
                 return bd[r][c];
 
-    // Check down-left
     for (r = 3; r < 6; r++)
         for (c = 0; c < 4; c++)
             if (chkLine(bd[r][c], bd[r - 1][c + 1], bd[r - 2][c + 2], bd[r - 3][c + 3]))
